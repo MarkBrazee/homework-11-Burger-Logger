20bcc59cdd065f33277902ef6782a6724f40daa5
Mark Brazee <m.brazee@comcast.net>
2020-11-02 09:06:38 -0700

    updated server-js

--- server.js
+++ server.js
@@ -1,3 +1,67 @@
+/*Psudeo code
+
+1. Create Github repo- Burger-Logger
+
+2. Create package JSON file
+
+3. Install express
+
+4. Create server.js file
+    - require express
+    - provide PORT
+    - variable for express
+    - set handlebars
+    - import routes
+    - start server to listen to client request
+
+5. Database setup
+    - create db folder
+        - create schema file
+            - create burgers_db
+            - switch to or use burgers_db
+                -burgers table
+                    - id as primary key
+                    - burger_name as a string
+                    - devoured: a boolean
+        - create seeds file
+            - burgers table
+    - run the SQL files
+        - command line tool mysql -u root -p
+            - command line source schema.sql
+            - command line source seeds.sql
+
+6. Create Config folder
+    - create connection.js file
+        - setup code to connect Node to MYSQL
+        - export the connection
+    - orm.js file
+        - create the methods to execute MySQL commands
+            - selectall()
+            - insertOne()
+            - updateOne()
+        - export the ORM object: modul.exports
+
+7. Create Model folder
+    - create burger.js file
+        - import orm.js
+        - create the code that will call the ORM functions using burger specific input for the ORM.
+        - export at the end of the burger.js file
+
+8. Create Contollers folder
+    - create burgers_controller.js file
+        - import Express
+        - import burger.js file
+    - create router for the app and export router at the end of the file
+
+9. Create Views folder
+    - create index.handlebars file
+        - create template that Handlebars can render onto
+        - create a button that will submit the user input into the database
+    - create Layouts folder
+        - create main.handlebars file
+            - set up file to use Handlebars
+*/
+
 var express = require("express");
 
 var PORT = process.env.PORT || 8080;
